e09636bfa60edb641d70bb5f833360d9e73ccc03
Lewis Lu <agooday32@gmail.com>
2021-06-12 19:35:21 +0800

    feat: move CRUD function to routes module

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+// const auth = require('./modules/auth')
+
+
+
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/users')
+
+// const { authenticator } = require('../middleware/auth')
+
+
+router.use('/todos', todos)
+router.use('/users', users)
+// router.use('/auth', auth)
+router.use('/', home)
+
+// 匯出路由器
+module.exports = router 
---
+++ routes/modules/users.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+const db = require('../../models')
+const User = db.User
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
+module.exports = router 
